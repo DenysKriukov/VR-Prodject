dcd2f8380bfc916a728fc2e270047f133808bd21
DenysKriukov <kryukov1978@gmail.com>
2022-11-12 22:17:16 +0200

    add new files

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 
 AOS.init();
+
+const headerMobileNav = document.querySelector(".open");
+
+const toggleBurgerMenu = () => {
+    headerMobileNav.classList.toggle(".open")
+};
+
+// const toggleBurgerMenu = () => {
+//     console. log("123");
+// };
