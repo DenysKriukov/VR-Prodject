deaec9f5e9e6af0d22220998256eae6c62813e61
DenysKriukov <kryukov1978@gmail.com>
2022-11-13 17:42:57 +0200

    add new files

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 
 AOS.init();
 
-const headerMobileNav = document.querySelector(".open");
+// const headerMobileNav = document.querySelector(".nav-wrap");
 
-const toggleBurgerMenu = () => {
-    headerMobileNav.classList.toggle(".open")
-};
+// const toggleBurgerMenu = () => {
+//     headerMobileNav.classList.toggle("open")
+// };
 
 // const toggleBurgerMenu = () => {
 //     console. log("123");
 // };
+const nawWrap = document.querySelector(".nav-wrap");
+const navButtonBurger = document.querySelector(".nav-button-burger");
+
+
+nawWrap.onclick = () => {
+    
+   nawWrap.classList.toggle('open')
+    
+}
