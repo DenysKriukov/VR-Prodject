2e1c064d930bc65d6130b48e2723fabd4586751a
DenysKriukov <kryukov1978@gmail.com>
2022-12-13 21:56:49 +0200

    add new files

---
+++ js/commentsStars.js
@@ -0,0 +1,40 @@
+import { COMMENTS } from "../mocks/comments.js"
+
+const commentsList = document.querySelector(".clients-say-comments");
+
+const renderItem = (comment) =>{
+    const liElement = document.createElement("li");
+    liElement.classList.add("clients-say-comments-name");
+
+    liElement.innerHTML = `
+    <li class="clients-say-comments-list">
+        <h5 class="clients-say-comments-name">${comment.genre}
+        <span class="clients-say-comments-name2"> star </span>  </h5>
+        <p class="clients-say-comments-text">${comment.comment}</p>
+    <div class="clients-info">
+        <img class="clients-img" src="../assets/img/${comment.img}.png" alt="client1 avatar">
+        <div class="clients-name">
+            <p>${comment.userName}</p>
+            <a href="mailto:">@${comment.userNickName}</a>
+        </div>
+    </div>
+    </li>
+    `;
+
+    return liElement;
+}
+
+const renderList = (element, list, className) =>{
+    const divElement = document.createElement("div");
+
+    const completeDivElement = list.reduce((divElement, item) =>{
+        divElement.appendChild(renderItem(item));
+        divElement.classList.add(className);
+
+        return divElement;
+    }, divElement);
+
+    element.appendChild(completeDivElement);
+}
+
+renderList(commentsList, COMMENTS, "clients-say-comments");
---
+++ mocks/comments.js
@@ -0,0 +1,30 @@
+export const COMMENTS =[{
+    id: "0",
+    genre: "rap",
+    comment: `“I know in real-time where the money is spent, and I don’t
+                have to lend out the company’s credit card anymore. What a
+                relief!”`,
+    img: "Comments1",
+    userName: "Nicki Minaj",
+    userNickName: "nicki.minaj",
+},
+{   
+    id: "1",
+    genre: "pop",
+    comment: `“I know in real-time where the money is spent, and I don’t
+                have to lend out the company’s credit card anymore. What a
+                relief!”`,
+    img: "Comments2",
+    userName: "Lady Gaga",
+    userNickName: "ledy.gaga",
+},
+{   
+    id: "2",
+    genre: "rock",
+    comment: `“I know in real-time where the money is spent, and I don’t
+                have to lend out the company’s credit card anymore. What a
+                relief!”`,
+    img: "Comments3",
+    userName: "Denny Hilguston",
+    userNickName: "denny.hill",
+},]
