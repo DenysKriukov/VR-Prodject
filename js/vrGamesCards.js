66bda4479fbc65cdac49217ea7ab657964db84e9
DenysKriukov <kryukov1978@gmail.com>
2023-01-01 19:51:50 +0200

    add new files

--- js/vrGamesCards.js
+++ js/vrGamesCards.js
@@ -7,8 +7,11 @@ const cardsList = document.querySelector(".games-section");
 const renderItem = (card) =>{
     const liElement = document.createElement("li");
 
+
+
     liElement.innerHTML = `
-        <img class="game-img" src="../assets/img/games/${card.img}.png" alt=${card.alt}
+    <li class="game-card" data-aos="flip-right">
+        <img class="game-img" src="../assets/img/games/${card.img}.png" alt=${card.alt}> 
         <div class="card-wrap-text">
             <div class="card-wrap-text-left">
                 <h4 class="game-name">${card.gameName}</h4>
@@ -19,6 +22,7 @@ const renderItem = (card) =>{
                 <p class="user-review">${card.userReview}</p>
             </diw>
         </div>
+    </li>
     `;
 
     return liElement;
@@ -26,7 +30,7 @@ const renderItem = (card) =>{
 
 const renderList = (element, list, className) => {
     const divElement = document.createElement("div");
-    // divElement.classList.add(className);
+      
     
 
     const completeDivElement = list.reduce((divElement, item) =>{
